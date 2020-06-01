154bb7b6a104a9ceb14d8f19bfe31eecdf058027
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-06-01 20:21:21 +0100

    1.2

--- src/ShowCountries.js
+++ src/ShowCountries.js
@@ -1,14 +1,13 @@
 import React from "react";
-
 const ShowCountries = (props) => {
 console.log("data:"+props.all);
     return ( 
     <div className="con">
         {props.all.map((country,index) => {
             return (
-            <div class="countries">
+            <div key={index} class="countries">
                 <div>
-                    <img class="image" src={country.flag}></img>
+                    <img class="image" src={country.flag} ></img>
                 </div>
                 <div>
                     <h2 class="nameC">{country.name}</h2>
